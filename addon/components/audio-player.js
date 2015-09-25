32bb1efd223aada39441c51b232de01225a1ec83
Loz Jackson <lozjackson@talktalk.net>
2015-09-25 16:44:32 +0100

    update audio-player component

--- addon/components/audio-player.js
+++ addon/components/audio-player.js
@@ -4,5 +4,11 @@ import layout from '../templates/components/audio-player';
 export default Ember.Component.extend({
   layout: layout,
 
+  tagName: 'audio',
+
+  attributeBindings: [ 'src', 'controls'],
+
+  controls: true,
+
   src: ''
 });
