4b25cf349e22b309ae1aff9a66cbe6876ee73bd4
Loz Jackson <lozjackson@talktalk.net>
2015-09-26 15:31:04 +0100

    add tone-generator component

---
+++ addon/components/tone-generator.js
@@ -0,0 +1,98 @@
+import Ember from 'ember';
+import layout from '../templates/components/tone-generator';
+
+export default Ember.Component.extend({
+
+  layout: layout,
+
+  classNames: [ 'ember-audio', 'tone-generator'],
+
+  audioService: Ember.inject.service(),
+
+  osc: null,
+
+  amp: null,
+
+  frequency: 500,
+
+  gain: 0.3,
+
+  linearGain: false,
+
+  output: null,
+
+  init() {
+    this._super(...arguments);
+    var context = this.get('audioService.audioContext');
+    var amp = context.createGain();
+
+    var output = this.get('output');
+    if (output) {
+      amp.connect(output);
+    }
+    this.setProperties({
+      audioContext: context,
+      amp: amp
+    });
+  },
+
+  volumeChanged: Ember.observer( 'gain', 'linearGain', function() {
+    var amp = this.get('amp');
+    var gain = this.get('gain');
+    var linearGain = this.get('linearGain');
+    amp.gain.value = (linearGain) ? gain : gain * gain;
+  }),
+
+  _frequency: Ember.computed( 'osc', 'frequency', {
+    get() {
+      var osc = this.get('osc');
+      if (!osc) {
+        return this.get('frequency');
+      }
+      return osc.frequency.value;
+    },
+    set( key, value ) {
+      var osc = this.get('osc');
+      if (osc) {
+        this.set('frequency', value);
+        return osc.frequency.value = value;
+      }
+    }
+  }),
+
+  start() {
+    var osc = this.get('osc');
+    if (!osc) {
+      var amp = this.get('amp');
+      var gain = this.get('gain');
+      var context = this.get('audioContext');
+      var frequency = this.get('frequency') || 440;
+      osc = context.createOscillator();
+      this.set('osc', osc);
+
+      osc.frequency.value = parseFloat(frequency);
+
+      amp.gain.value = gain;
+      osc.connect(amp);
+      osc.start();
+    }
+  },
+
+  stop() {
+    var osc = this.get('osc');
+    if (osc) {
+      osc.stop();
+      this.set('osc', null);
+    }
+  },
+
+  actions: {
+    start() {
+      this.start();
+    },
+
+    stop() {
+      this.stop();
+    }
+  }
+});
---
+++ tests/integration/components/tone-generator-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('tone-generator', 'Integration | Component | tone generator', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{tone-generator}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#tone-generator}}
+      template block text
+    {{/tone-generator}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
