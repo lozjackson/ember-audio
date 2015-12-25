ab20f7ab689de1b537295b13749defc065b4027a
Loz Jackson <lozjackson@talktalk.net>
2015-12-25 22:47:30 +0000

    update  audio-bus mixin

--- addon/mixins/audio-bus.js
+++ addon/mixins/audio-bus.js
@@ -3,6 +3,10 @@
 */
 import Ember from 'ember';
 
+var computed = Ember.computed;
+var alias = computed.alias;
+var observer = Ember.observer;
+
 /**
   ## AudioBusMixin
 
@@ -51,66 +55,86 @@ export default Ember.Mixin.create({
   gain: 1,
 
   /**
+    Alias of `audioService.audioContext`
+
+    @property audioContext
+    @type {Object}
+    @private
+  */
+  audioContext: alias('audioService.audioContext'),
+
+  /**
+    Computed Property.  The gain/polarity of the bus.
+
+    @property _gain
+    @type {Number}
+    @private
+  */
+  _gain: computed('gain', 'polarity', function () {
+    var {gain, polarity} = this.getProperties('gain', 'polarity');
+    return (polarity) ? gain : gain * -1;
+  }),
+
+  /**
+    Create the bus and set the gain.
+    
     @method init
     @private
   */
   init() {
     this._super(...arguments);
-    var bus = this.createBus();
-    if ( bus ) {
-      this.set( 'bus', bus );
-    }
+    this.createBus();
+    this.setGain();
   },
 
   /**
-    Create the bus and set the default parameters.
+    Create the bus.
 
     @method createBus
     @private
   */
   createBus() {
-
-    var audioContext = this.get('audioService.audioContext');
-    var polarity = this.get('polarity');
-    var mute = this.get('mute');
+    var audioContext = this.get('audioContext');
     var bus = audioContext.createGain();
-    if ( mute ) {
-      bus.gain.value = 0;
-    } else {
-      this.setPolarity(polarity);
-    }
+    this.set( 'bus', bus );
     return bus;
   },
 
-  setPolarity(polarity) {
-    var bus = this.get('bus');
-    var gain = parseFloat(this.get('gain'));
-    if ( bus ) {
-      bus.gain.value = (polarity) ? gain : gain * -1;
+  /**
+    Set the gain of the bus.
+
+    @method setGain
+    @param {Number} gain
+    @private
+  */
+  setGain(gain) {
+    const { bus, _gain, mute } = this.getProperties('bus', '_gain', 'mute');
+    if (isNaN(gain)) {
+      gain = parseFloat(_gain);
+    }
+    if (bus) {
+      bus.gain.value = (mute) ? 0 : gain;
     }
   },
 
-  gainChanged: Ember.observer('gain', function() {
-    var bus = this.get('bus');
-    var gain = this.get('gain');
-    var polarity = this.get('polarity');
-    bus.gain.value = (polarity) ? gain : gain * -1;
+  /**
+    @event gainChanged
+  */
+  gainChanged: observer('gain', function() {
+    this.setGain();
   }),
 
-  polarityChanged: Ember.observer('polarity', function() {
-    var polarity = this.get('polarity');
-    this.setPolarity(polarity);
+  /**
+    @event polarityChanged
+  */
+  polarityChanged: observer('polarity', function() {
+    this.setGain();
   }),
 
-  muteChanged: Ember.observer('mute', function() {
-    var mute = this.get('mute');
-    var gain = parseFloat(this.get('gain'));
-    if ( mute ) {
-      var bus = this.get('bus');
-      bus.gain.value = 0;
-    } else {
-      var polarity = this.get('polarity');
-      this.setPolarity(polarity);
-    }
+  /**
+    @event muteChanged
+  */
+  muteChanged: observer('mute', function() {
+    this.setGain();
   })
 });
--- tests/unit/mixins/audio-bus-test.js
+++ tests/unit/mixins/audio-bus-test.js
@@ -4,9 +4,190 @@ import { module, test } from 'qunit';
 
 module('Unit | Mixin | audio bus');
 
+// var AudioService = {
+//   audioContext: {
+//     createGain: function () {
+//       return {
+//         gain: {
+//           value: 1
+//         }
+//       };
+//     }
+//   }
+// };
+var AudioContext = window.AudioContext;
+var AudioService = {
+  audioContext: new AudioContext()
+};
+
 // Replace this with your real tests.
 test('it works', function(assert) {
   var AudioBusObject = Ember.Object.extend(AudioBusMixin);
-  var subject = AudioBusObject.create();
+  var subject = AudioBusObject.create({ audioService: AudioService });
   assert.ok(subject);
+});
+
+// bus
+
+test('polarity should be true', function(assert) {
+  assert.expect(1);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('polarity'), true, `'polarity' should be true`);
+});
+
+test('mute should be false', function(assert) {
+  assert.expect(1);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('mute'), false, `'mute' should be false`);
+});
+
+test('gain should be 1', function(assert) {
+  assert.expect(1);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('gain'), 1, `'gain' should be 1`);
+});
+
+test('bus.gain.value should be 0.5', function(assert) {
+  assert.expect(1);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({
+    audioService: AudioService,
+    gain: 0.5
+  });
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+});
+
+test('audioContext alias', function(assert) {
+  assert.expect(1);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: {
+    audioContext: {
+      name: 'audio-context',
+      createGain: function () {
+      }
+    }
+  }});
+  assert.equal(subject.get('audioContext.name'), 'audio-context', `'audioContext.name' should be 'audio-context'`);
+});
+
+test('_gain computed property', function (assert) {
+  assert.expect(1);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('_gain'), 1, `'_gain' should be 1`);
+});
+
+test('_gain computed property - inverted polarity', function (assert) {
+  // assert.expect(2);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({
+    audioService: AudioService ,
+    polarity: false
+  });
+  assert.equal(subject.get('_gain'), -1, `'_gain' should be -1`);
+});
+
+test('createBus method', function(assert) {
+  assert.expect(2);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  var bus = subject.createBus();
+  assert.equal(bus.gain.value, 1, `'bus.gain.value' should be 1`);
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+});
+
+test('setGain method', function(assert) {
+  assert.expect(2);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+  subject.setGain(0.5);
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+});
+
+test('gainChanged', function(assert) {
+  assert.expect(2);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+  subject.setGain(0.5);
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+});
+
+test('polarityChanged', function(assert) {
+  assert.expect(3);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+
+  subject.set('polarity', false);
+  assert.equal(subject.get('bus.gain.value'), -1, `'bus.gain.value' should be -1`);
+
+  subject.set('polarity', true);
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+});
+
+test('polarity and gain changed', function(assert) {
+  assert.expect(4);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+
+  subject.set('gain', 0.5);
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+
+  subject.set('polarity', false);
+  assert.equal(subject.get('bus.gain.value'), -0.5, `'bus.gain.value' should be -0.5`);
+
+  subject.set('polarity', true);
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+});
+
+test('muteChanged', function(assert) {
+  assert.expect(3);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({ audioService: AudioService });
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+
+  subject.set('mute', true);
+  assert.equal(subject.get('bus.gain.value'), 0, `'bus.gain.value' should be 0`);
+
+  subject.set('mute', false);
+  assert.equal(subject.get('bus.gain.value'), 1, `'bus.gain.value' should be 1`);
+});
+
+test('muteChanged with gain at half', function(assert) {
+  assert.expect(3);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({
+    audioService: AudioService,
+    gain: 0.5
+  });
+
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+
+  subject.set('mute', true);
+  assert.equal(subject.get('bus.gain.value'), 0, `'bus.gain.value' should be 0`);
+
+  subject.set('mute', false);
+  assert.equal(subject.get('bus.gain.value'), 0.5, `'bus.gain.value' should be 0.5`);
+});
+
+test('muteChanged with polarity inverted', function(assert) {
+  assert.expect(3);
+  var AudioBusObject = Ember.Object.extend(AudioBusMixin);
+  var subject = AudioBusObject.create({
+    audioService: AudioService,
+    polarity: false
+  });
+  assert.equal(subject.get('bus.gain.value'), -1, `'bus.gain.value' should be -1`);
+
+  subject.set('mute', true);
+  assert.equal(subject.get('bus.gain.value'), 0, `'bus.gain.value' should be 0`);
+
+  subject.set('mute', false);
+  assert.equal(subject.get('bus.gain.value'), -1, `'bus.gain.value' should be -1`);
 });
