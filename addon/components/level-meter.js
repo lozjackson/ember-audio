68f2e054ed1000799350fd920a0cde7c20497e94
Loz Jackson <lozjackson@talktalk.net>
2018-03-08 23:02:17 +0000

    Update level meter component

--- addon/components/level-meter.js
+++ addon/components/level-meter.js
@@ -71,43 +71,43 @@ export default Component.extend(io, ProcessorMixin, {
 
   didInsertElement() {
     var canvas = this.$()[0];
-  	var ctx = canvas.getContext('2d');
-  	var w = canvas.width;
-  	var h = canvas.height;
+    var ctx = canvas.getContext('2d');
+    var w = canvas.width;
+    var h = canvas.height;
     var processor = this.get('processor');
 
-  	//fill the canvas first
-  	ctx.fillStyle = '#555';
-  	ctx.fillRect(0,0,w,h);
+    //fill the canvas first
+    ctx.fillStyle = '#555';
+    ctx.fillRect(0,0,w,h);
 
     processor.onaudioprocess = function(e){
-  		var out = e.outputBuffer.getChannelData(0);
-  		var int = e.inputBuffer.getChannelData(0);
-  		var max = 0;
+      var out = e.outputBuffer.getChannelData(0);
+      var int = e.inputBuffer.getChannelData(0);
+      var max = 0;
 
-  		for(var i = 0; i < int.length; i++){
-  			out[i] = 0;//prevent feedback and we only need the input data
-  			max = int[i] > max ? int[i] : max;
-  		}
-  		//convert from magitude to decibel
-  		var db = 20*Math.log(Math.max(max,Math.pow(10,-72/20)))/Math.LN10;
-  		//It's time to draw on the canvas
-  		//create the gradient
-  		var grad = ctx.createLinearGradient(w/10,h*0.2,w/10,h*0.95);
-  		grad.addColorStop(0,'red');
-  		grad.addColorStop(-6/-72,'yellow');
-  		grad.addColorStop(1,'green');
-  		//fill the background
-  		ctx.fillStyle = '#555';
-  		ctx.fillRect(0,0,w,h);
-  		ctx.fillStyle = grad;
-  		//draw the rectangle
-  		ctx.fillRect(w/10,h*0.8*(db/-72),w*8/10,(h*0.95)-h*0.8*(db/-72));
-  		//draw the text out
-  		ctx.fillStyle="white";
-  		ctx.font = "Arial 12pt";
-  		ctx.textAlign = "center";
-  		ctx.fillText(Math.round(db*100)/100+' dB',w/2,h-h*0.025);
-  	};
-  },
+      for(var i = 0; i < int.length; i++){
+        out[i] = 0;//prevent feedback and we only need the input data
+        max = int[i] > max ? int[i] : max;
+      }
+      //convert from magitude to decibel
+      var db = 20*Math.log(Math.max(max,Math.pow(10,-72/20)))/Math.LN10;
+      //It's time to draw on the canvas
+      //create the gradient
+      var grad = ctx.createLinearGradient(w/10,h*0.2,w/10,h*0.95);
+      grad.addColorStop(0,'red');
+      grad.addColorStop(-6/-72,'yellow');
+      grad.addColorStop(1,'green');
+      //fill the background
+      ctx.fillStyle = '#555';
+      ctx.fillRect(0,0,w,h);
+      ctx.fillStyle = grad;
+      //draw the rectangle
+      ctx.fillRect(w/10,h*0.8*(db/-72),w*8/10,(h*0.95)-h*0.8*(db/-72));
+      //draw the text out
+      ctx.fillStyle="white";
+      ctx.font = "Arial 12pt";
+      ctx.textAlign = "center";
+      ctx.fillText(Math.round(db*100)/100+' dB',w/2,h-h*0.025);
+    };
+  }
 });
